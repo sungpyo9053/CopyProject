e277d8011d04f15f1f0a5e7edbcc70d6af91cb1e
rebuild99 <njk118@naver.com>
2022-03-02 22:04:52 +0900

    Profile 작업

--- copyProject/Sources/Entry/AppDelegate.swift
+++ copyProject/Sources/Entry/AppDelegate.swift
@@ -6,15 +6,15 @@
 //
 
 import UIKit
-import Firebase
-import GoogleSignIn
+//import Firebase
+//import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        FirebaseApp.configure()
+        //FirebaseApp.configure()
         
         return true
     }
